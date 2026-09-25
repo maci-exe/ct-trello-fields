fb9e9f730fb30a556a38a67b8dcd15dedf7f892e
maci-exe <macimitat@gmail.com>
2026-09-25 10:43:12 +0200

    Create powerup.js

---
+++ powerup.js
@@ -0,0 +1,8 @@
+window.TrelloPowerUp.initialize({
+    'card-badges': function(t, opts) {
+        return [{
+            text: 'Rang: Sergeant',
+            color: 'green'
+        }];
+    }
+});
